151edb0e35b3203c75468951d27fbef6f8a7de46
nuwan123-png <dinidunuwanakalanka@gmail.com>
2025-07-02 20:57:02 +0530

    Add files via upload

---
+++ P2_form/P2_form/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace P2_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            textBox3 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            comboBox1 = new ComboBox();
+            textBox4 = new TextBox();
+            comboBox2 = new ComboBox();
+            submit = new Button();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(192, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 20);
+            label1.TabIndex = 0;
+            label1.Text = "first name :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(192, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 20);
+            label2.TabIndex = 1;
+            label2.Text = "last name :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(192, 138);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 20);
+            label3.TabIndex = 2;
+            label3.Text = "gender :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(192, 175);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 20);
+            label4.TabIndex = 3;
+            label4.Text = "language :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(192, 206);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 20);
+            label5.TabIndex = 4;
+            label5.Text = "email :";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(192, 247);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 20);
+            label6.TabIndex = 5;
+            label6.Text = "address :";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(192, 396);
+            label7.Name = "label7";
+            label7.Size = new Size(36, 20);
+            label7.TabIndex = 6;
+            label7.Text = "zip :";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(192, 362);
+            label8.Name = "label8";
+            label8.Size = new Size(48, 20);
+            label8.TabIndex = 7;
+            label8.Text = "state :";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(192, 437);
+            label9.Name = "label9";
+            label9.Size = new Size(116, 20);
+            label9.TabIndex = 8;
+            label9.Text = "credit card type:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(331, 64);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(331, 102);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(330, 138);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 24);
+            radioButton1.TabIndex = 11;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(406, 138);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(76, 24);
+            radioButton2.TabIndex = 12;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(330, 205);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 13;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(330, 175);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(73, 24);
+            checkBox1.TabIndex = 14;
+            checkBox1.Text = "telugu";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(420, 175);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(78, 24);
+            checkBox2.TabIndex = 15;
+            checkBox2.Text = "english";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(504, 175);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(64, 24);
+            checkBox3.TabIndex = 16;
+            checkBox3.Text = "hindi";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(331, 359);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(330, 396);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 18;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(330, 429);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 19;
+            // 
+            // submit
+            // 
+            submit.Location = new Point(381, 479);
+            submit.Name = "submit";
+            submit.Size = new Size(94, 29);
+            submit.TabIndex = 20;
+            submit.Text = "Submit";
+            submit.UseVisualStyleBackColor = true;
+            submit.Click += button1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(331, 248);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(252, 95);
+            richTextBox1.TabIndex = 21;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuBar;
+            ClientSize = new Size(800, 527);
+            Controls.Add(richTextBox1);
+            Controls.Add(submit);
+            Controls.Add(comboBox2);
+            Controls.Add(textBox4);
+            Controls.Add(comboBox1);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private TextBox textBox3;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private ComboBox comboBox1;
+        private TextBox textBox4;
+        private ComboBox comboBox2;
+        private Button submit;
+        private RichTextBox richTextBox1;
+    }
+}
